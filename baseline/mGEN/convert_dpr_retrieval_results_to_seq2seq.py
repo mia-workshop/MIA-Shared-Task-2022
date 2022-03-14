2bf1849494f85de0e95e89650c947d22583bd3e3
AkariAsai <akari.1311aquamarine4@gmail.com>
2022-03-14 16:50:25 -0700

    update script

--- baseline/mGEN/convert_dpr_retrieval_results_to_seq2seq.py
+++ baseline/mGEN/convert_dpr_retrieval_results_to_seq2seq.py
@@ -16,14 +16,14 @@ def read_jsonlines(eval_file_name):
     return lines
 
 
-def load_dpr_results(pred_results, top_n=5, split="train", lang_balance=False, orig_xor_eng_ids=None, lang_dict=None):
+def load_dpr_results(pred_results, top_n=5, split="train"):
     q_c_a = []
     has_answer = 0
     for item in tqdm(pred_results):
         question = item["question"]
         answers = item["answers"]
-        q_id = item["q_id"]
         ctxs = item["ctxs"]
+        lang = item["lang"]
         for ctx in ctxs:
             if ctx["has_answer"] == True:
                 has_answer += 1
@@ -54,18 +54,6 @@ def load_dpr_results(pred_results, top_n=5, split="train", lang_balance=False, o
             context += "<{0}: {1}> ".format(idx, para["title"])
             context += para["text"]
 
-        if split != "train":
-            lang = item["lang"]
-        else:
-            if item["lang"] == "NONE":
-                lang = "en"
-            # XOR full train data come with English answers, so we set the target language to English.
-            elif item["lang"] != "NONE" and q_id in orig_xor_eng_ids:
-                lang = "en"
-            elif item["lang"] != "NONE" and q_id not in orig_xor_eng_ids:
-                lang = item["lang"]
-            else:
-                raise NotImplementedError
 
         q_c_a.append({"question": question, "answers": answers,
                       "context": context, "lang": lang})
@@ -82,9 +70,6 @@ def main():
     parser.add_argument("--output_dir", default=None, type=str)
     parser.add_argument("--top_n", default=5, type=int)
     parser.add_argument("--add_lang", action="store_true")
-    parser.add_argument("--xor_engspan_train", default=None, type=str)
-    parser.add_argument("--xor_full_train", default=None, type=str)
-    parser.add_argument("--xor_full_dev", default=None, type=str)
 
     args = parser.parse_args()
 
@@ -100,15 +85,7 @@ def main():
         test_data = json.load(open(args.test_fp))
 
     if args.train_fp is not None:
-        if args.add_lang is True:
-            orig_xor_eng_ids = [item["id"]
-                                for item in read_jsonlines(args.xor_engspan_train)]
-            lang_dict_train = {item["id"]: item["lang"]
-                               for item in read_jsonlines(args.xor_full_train)}
-            s2s_train = load_dpr_results(
-                train_data, top_n=args.top_n, orig_xor_eng_ids=orig_xor_eng_ids, lang_dict=lang_dict_train)
-        else:
-            s2s_train = load_dpr_results(train_data, top_n=args.top_n)
+        s2s_train = load_dpr_results(train_data, top_n=args.top_n)
         source_f_train = open(os.path.join(
             args.output_dir, "train.source"), "w")
         target_f_train = open(os.path.join(
@@ -127,14 +104,7 @@ def main():
         target_f_train.close()
 
     if args.dev_fp is not None:
-        if args.add_lang is True:
-            lang_dict_dev = {item["id"]: item["lang"]
-                             for item in read_jsonlines(args.xor_full_dev)}
-            s2s_dev = load_dpr_results(
-                dev_data, top_n=args.top_n, split="dev", lang_dict=lang_dict_dev)
-        else:
-            s2s_dev = load_dpr_results(
-                dev_data, top_n=args.top_n, split="dev")
+        s2s_dev = load_dpr_results(dev_data, top_n=args.top_n, split="dev")
         source_f_val = open(os.path.join(args.output_dir, "val.source"), "w")
         target_f_val = open(os.path.join(args.output_dir, "val.target"), "w")
 
@@ -171,7 +141,6 @@ def main():
 
     if args.test_fp is not None:
         s2s_test = load_dpr_results(test_data, top_n=args.top_n, split="test")
-
         source_f_test = open(os.path.join(args.output_dir, "test.source"), "w")
         target_f_test = open(os.path.join(args.output_dir, "test.target"), "w")
 
