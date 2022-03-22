876ea46060a52a1ad1c9437dff817f9af7bcb74b
AkariAsai <akari.1311aquamarine4@gmail.com>
2022-03-21 18:45:47 -0700

    modify mdpr adv data creation script

--- baseline/mDPR/create_adverarial_data.py
+++ baseline/mDPR/create_adverarial_data.py
@@ -1,12 +1,8 @@
 import json
 import random
 import argparse
-import csv
-import os
 from tqdm import tqdm
 import jsonlines
-import ast
-import re
 import numpy as np
 from collections import Counter
 
@@ -18,8 +14,6 @@ def read_jsonlines(eval_file_name):
             lines.append(obj)
     return lines
 
-
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--qa_file", default=None, type=str)
@@ -29,7 +23,7 @@ def main():
 
     args = parser.parse_args()
 
-    orig_open_data = read_jsonlines(args.input_fn)
+    orig_open_data = read_jsonlines(args.qa_file)
     orig_train_data = json.load(open(args.orig_train_file))
     train_retr_results = json.load(open(args.train_retr_results))
 
@@ -40,8 +34,8 @@ def main():
     skip = 0
     p_count = []
     n_count = []
-    for item in orig_open_data:
-        qid = item["question"]
+    for item in tqdm(orig_open_data):
+        qid = item["id"]
         retr_results = qid2retr_results[qid]
         positives = []
         negatives = []
@@ -51,17 +45,18 @@ def main():
             else:
                 negatives.append(ctx)
         new_train_sample = qid2orig_data[qid] if qid in qid2orig_data else {}
-        if qid not in orig_train_data:
+
+        if qid not in qid2orig_data:
             new_train_sample["question"] = item["question"]
             new_train_sample["answers"] = item["answers"]
-            new_train_sample["q_id"] = item["q_id"]
+            new_train_sample["q_id"] = item["id"]
             new_train_sample["negative_ctxs"] = []
             new_train_sample["hard_negative_ctxs"] = []
             new_train_sample["positive_ctxs"] = []
 
         new_train_sample["positive_ctxs"] += positives
         hard_negatives_all = negatives + new_train_sample["hard_negative_ctxs"]
-        sample_indices =  random.sample(range(hard_negatives_all), k=max(50, len(hard_negatives_all)))
+        sample_indices =  random.sample(range(len(hard_negatives_all)), k=min(50, len(hard_negatives_all)))
         hard_negatives = [ctx for idx, ctx in enumerate(hard_negatives_all) if idx in sample_indices]
         new_train_sample["hard_negative_ctxs"] = hard_negatives
 
@@ -75,10 +70,10 @@ def main():
             assert "answers" in new_train_sample
             assert "q_id" in new_train_sample
             new_data.append(new_train_sample)
-    print("processed {0} examples: {1} final examples.".format(len(orig_open_data),len(orig_open_data) - skip ))
-    print("avg positive ctxs number: {0}, avg negative ctxs number:{1}".formatlen(np.mean(p_count), np.mean(n_count)))
     with open(args.output_fn, 'w') as outfile:
         json.dump(new_data, outfile)
+    print("processed {0} examples: {1} final examples.".format(len(orig_open_data),len(orig_open_data) - skip ))
+    print("avg positive ctxs number: {0}, avg negative ctxs number:{1}".format(np.mean(p_count), np.mean(n_count)))
 
 if __name__=="__main__":
     main()
